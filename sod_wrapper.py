3a72eae0c8563e563ee8ad9ac5f021d823ec5ca0
Ben Johnson <bkj.322@gmail.com>
2021-01-20 01:16:52 -0500

    bugfixes

--- sod_wrapper.py
+++ sod_wrapper.py
@@ -35,33 +35,32 @@ class SODWrapper(Dataset):
     Xf = torch.ones(c, self.frame_scale * h, self.frame_scale * w).float()
     yf = torch.zeros(1, self.frame_scale * h, self.frame_scale * w).long() - 1
 
-    i = 0
+    success = 0
+    failure = 0
     while True:
-      h_off = rng.choice((self.frame_scale - 1) * h)
-      w_off = rng.choice((self.frame_scale - 1) * w)
+      h_off = np.random.choice((self.frame_scale - 1) * h)
+      w_off = np.random.choice((self.frame_scale - 1) * w)
       
       no_overlap = (yf[:, h_off:h_off + h, w_off:w_off + w] == -1).all()
       if no_overlap:
-        Xf[:, h_off:h_off + h, w_off:w_off + w] = X[i]
-        yf[:, h_off:h_off + h, w_off:w_off + w] = y[i]
-        i += 1
+        Xf[:, h_off:h_off + h, w_off:w_off + w] = X[success]
+        yf[:, h_off:h_off + h, w_off:w_off + w] = y[success]
+        success += 1
       else:
-        failures += 1
+        failure += 1
       
-      if i == len(X):
-        return Xy, yf
-      elif failures == max_failures:
+      if success == len(X):
+        return Xf, yf
+      elif failure == max_failures:
         return None, None
     
   def __getitem__(self, frame_idx):
-    rng = np.random if self.seed is None else np.random.RandomState(seed=self.seed + frame_idx)
-    
     tmp  = [self.dataset[i] for i in self.idxs[frame_idx]]
     X, y = zip(*tmp)
     
-    Xy, yf = None
+    Xf, yf = None, None
     while Xf is None:
-      Xy, yf = self._try_sample(X, y)
+      Xf, yf = self._try_sample(X, y)
     
     return Xf, yf
 
--- test.py
+++ test.py
@@ -1,5 +1,3 @@
-from tqdm import tqdm
-
 from torchvision import datasets
 from torchvision import transforms as T
 
@@ -10,12 +8,7 @@ from sod_wrapper import SODWrapper
 
 # obs_dataset = datasets.Omniglot(root='./data', background=True, download=True, transform=T.ToTensor())
 obs_dataset = datasets.CIFAR10(root='./data', train=True, download=True, transform=T.ToTensor())
+
 sod_dataset = SODWrapper(obs_dataset, obs_per_frame=5)
-# _ = plt.imshow(sod_dataset[0][0].numpy().transpose(1, 2, 0), cmap='gray')
-# show_plot()
-
-# for _ in tqdm(obs_dataset):
-#   pass
-
-for _ in tqdm(sod_dataset):
-  pass
+_ = plt.imshow(sod_dataset[0][0].numpy().transpose(1, 2, 0), cmap='gray')
+show_plot()
